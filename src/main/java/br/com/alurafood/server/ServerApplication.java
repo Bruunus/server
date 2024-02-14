31048aff4da573ab713e8a50c1327fc1b07ed2a5
Bruno <brunus.desenvolvimentobackfront@gmail.com>
2024-02-14 10:34:23 -0300

    teste git

--- src/main/java/br/com/alurafood/server/ServerApplication.java
+++ src/main/java/br/com/alurafood/server/ServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class ServerApplication {
 
 	public static void main(String[] args) {
+		// github
 		SpringApplication.run(ServerApplication.class, args);
 		System.out.println("Servidor Eureka Register Server rodando com sucesso !!!");
 	}
